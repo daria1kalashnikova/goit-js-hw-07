1fa6aa983aa5bc1ce73bc849fdbed34cd8c5376a
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-11-19 01:45:02 +0200

    task 5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+const onBtnClick = event => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+};
+
+btnEl.addEventListener('click', onBtnClick);
