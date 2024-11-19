3336cd0d48874c97f415c8f3911dfd08911b75e9
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-11-19 02:25:12 +0200

    task 3 remake

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,7 @@
 const inputName = document.querySelector('#name-input');
 const spanName = document.querySelector('#name-output');
 
-inputName.addEventListener('input', event => {
-  const name = inputName.value;
-  const readyName = name.trim();
-
-  if (spanName.textContent === '') {
-    spanName.textContent = 'Anonymous';
-  } else spanName.textContent = readyName;
+inputName.addEventListener('input', () => {
+  const readyName = inputName.value.trim();
+  spanName.textContent = readyName === '' ? 'Anonymous' : readyName;
 });
